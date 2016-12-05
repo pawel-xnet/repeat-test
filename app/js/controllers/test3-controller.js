79f1747ee90ca6183311d7abb26e01586a41a630
user <szalewskipawel@gmail.com>
2016-12-05 18:27:20 +0100

    flex learn 1

--- app/js/controllers/test3-controller.js
+++ app/js/controllers/test3-controller.js
@@ -2,23 +2,6 @@ angular.module('NoteWrangler')
 .controller('Test3Controller', function($scope, $location, PageService){
     var vm = this;
     
-    vm.array1 = [
-        {
-            name: 'a11',
-            age: '11'
-        },{
-            name: 'a22',
-            age: '22'
-        },{
-            name: 'a33',
-            age: '33'
-        },{
-            name: 'a44',
-            age: '44'
-        },{
-            name: 'a55',
-            age: '55'
-        }
-    ];
+
     
 });
